028dfc66d56bb81ad2a78c70fe4960f4b7ec27a5
Shahbaz Ali <alishahbaz617@gmail.com>
2020-06-18 00:26:14 +0530

    Changed secret key loc

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'],
+SECRET_KEY = 'rtsf2lk+sw@ap$qv4ps&#&na6c1hp5+91nly(^1@lez4c#g#!)',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
