ab39e270ba11e674069cbf7478ecbe67ec3c3b41
SarfarazTargaryen <alimohdsarfaraz@gmail.com>
2020-06-25 00:05:06 +0530

    Machine Learning Tool debugged

--- backend/titanic.py
+++ backend/titanic.py
@@ -1,16 +1,16 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+def learn(PassengerClass,No_siblings,No_children,female,male) :
 
-y = train_data["Survived"]
+    import pandas as pd
+    from sklearn.ensemble import RandomForestClassifier
 
-features = ["Pclass", "Sex", "SibSp", "Parch"]
-X = pd.get_dummies(train_data[features])
-X_test = pd.get_dummies(test_data[features])
+    data=[{"PassC":PassengerClass,"Sib":No_siblings,"chi":No_children,"fem":female,"mal":male}]
+    test=pd.DataFrame(data)
+    train_data = pd.read_csv("C:/Users/alimo/Desktop/titanic/train.csv")
+    y = train_data["Survived"]
 
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-model.fit(X, y)
-predictions = model.predict(X_test)
-
-output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('my_submission.csv', index=False)
-print("Your submission was successfully saved!")
+    features = ["Pclass", "Sex", "SibSp", "Parch"]
+    X = pd.get_dummies(train_data[features])
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+    model.fit(X, y)
+    predictions = model.predict(test)
+    return(predictions)
